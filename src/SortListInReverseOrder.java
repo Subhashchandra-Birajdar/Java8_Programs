6b654386c67c505d9ab57e9a2b862139ea9db6ca
adhi200208 <adarshchoudary111@gmail.com>
2024-09-21 08:39:29 +0530

    Sort list in reverse order using comparator

--- src/SortListInReverseOrder.java
+++ src/SortListInReverseOrder.java
@@ -3,6 +3,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SortListInReverseOrder {
+
     public static void main(String[] args) {
         List<Integer> anyList = Arrays.asList(10, 20, 30, 40, 5, 23, 23, 1, 2, 56, 7, 78, 565, 3, 343, 2);
 
