9348e226c9165ebe5d53fbe5cbd4c44fff48c412
adhi200208 <adarshchoudary111@gmail.com>
2024-09-21 08:28:53 +0530

    Frequency of each character in string

--- src/FrequencyOfEachCharInString.java
+++ src/FrequencyOfEachCharInString.java
@@ -7,10 +7,11 @@ public class FrequencyOfEachCharInString {
         String str = "subhashchandra";
 
         Map<Character, Long> collect = str.chars().
-                mapToObj(c ->(char) c)
+                mapToObj(c ->(char) c) // Convert int to char
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-
+      // Function.identity() is useful when you want to group or process elements
+        // without any transformation, as it allows you to use the elements themselves as keys or values.
         collect.forEach((Character,count)->
                 System.out.println(Character+" :"+count));
     }
